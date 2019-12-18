8ee10f590ec93a3a02399d0886dd7c8b2924693a
Rukkie555 <terryfrank555@gmail.com>
2019-12-19 00:06:59 +0100

    moment.js now works

---
+++ src/components/home/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-app-bar flat max-width="100vw" :fixed="true" color="transparent" dark>
+      <v-toolbar-title>
+        <p class="time shadow" v-text="currentTime"></p>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  name: "nav",
+
+  data: () => ({
+    currentTime: null
+  }),
+
+  methods: {
+    updateCurrentTime() {
+      this.currentTime = moment().format("LTS");
+    }
+  },
+
+  created() {
+    this.currentTime = moment().format("LTS");
+    setInterval(() => this.updateCurrentTime(), 1 * 1000);
+  }
+};
+</script>
