5cdf658c332968dda9b7a91d58a4bf52e44f38b6
Rukkie555 <terryfrank555@gmail.com>
2019-12-18 08:19:14 +0100

    looks good on mobile

--- src/components/dashboard/Todo.vue
+++ src/components/dashboard/Todo.vue
@@ -22,63 +22,61 @@
       </v-flex>
     </v-layout>
 
-    <v-container class="my-5 mt-12">
-      <v-layout row wrap>
-        <Dialog class="ml-3 mb-4" />
-      </v-layout>
+    <v-layout row wrap>
+      <Dialog class="ml-3 mb-4" />
+    </v-layout>
 
-      <transition-group
-        enter-active-class="animated bounceInUp"
-        leave-active-class="animated bounceOutDown"
+    <transition-group
+      enter-active-class="animated bounceInUp"
+      leave-active-class="animated bounceOutDown"
+    >
+      <v-card
+        class="pa-0 ma-0"
+        flat
+        width="100%"
+        tile
+        v-for="todo in order"
+        :key="todo.title"
+        :class="`todo ${todo.state}`"
       >
-        <v-card
-          class="pa-0 ma-0"
-          flat
-          tile
-          v-for="todo in order"
-          :key="todo.title"
-          :class="`todo ${todo.state}`"
-        >
-          <v-card-text class="ma-0 pa-0">
-            <v-layout class="my-0" row wrap>
-              <v-flex xs1 sm1 md1 lg1 xl1>
-                <v-layout class="pl-6 pt-4" justify-start>
-                  <v-btn left @click="complete(todo)" x-small icon>
-                    <v-icon>mdi-clipboard-list</v-icon>
-                  </v-btn>
-                </v-layout>
-              </v-flex>
-              <v-flex class="pt-4" xs8 sm4 md4 lg4 xl4>
-                <div class="grey--text text--darken-2">
-                  {{ todo.title }}
-                </div>
-              </v-flex>
-              <v-flex class="px-6 py-2 pt-4" xs3 sm3 md3 lg3 xl3>
-                <div class="grey--text text--darken-2">
-                  {{ todo.date }}
-                </div>
-              </v-flex>
-              <v-flex xs1 sm1 md1 lg1 xl1>
-                <v-chip
-                  small
-                  ripple
-                  :class="`${todo.state} white--text caption my-2`"
-                  >{{ todo.state }}</v-chip
-                >
-              </v-flex>
-              <v-flex class="pr-4 pt-2" xs2 sm2 md2 lg2 xl2>
-                <v-layout justify-end>
-                  <v-btn depressed @click="deleteTodo(todo)" icon>
-                    <v-icon class="red--text text--darken-5">mdi-minus-circle</v-icon>
-                  </v-btn>
-                </v-layout>
-              </v-flex>
-            </v-layout>
-            <v-divider></v-divider>
-          </v-card-text>
-        </v-card>
-      </transition-group>
-    </v-container>
+        <v-card-text class="ma-0 pax-1">
+          <v-layout class="my-0" row wrap>
+            <v-flex xs1 sm1 md1 lg1 xl1>
+              <v-btn class="ma-0 pa-0" left @click="complete(todo)" x-small icon>
+                  <v-icon>mdi-check</v-icon>
+                </v-btn>
+            </v-flex>
+
+            <v-flex xs4 sm4 md4 lg4 xl4>
+              <div class="grey--text text--darken-2">
+                <span class="body-2 ma-0 pa-0">{{ todo.title }}</span>
+              </div>
+            </v-flex>
+
+            <v-flex xs3 sm3 md3 lg3 xl3>
+              <div class="grey--text text--darken-2">
+                <span class="body-2 ma-0 pa-0">{{ todo.date }}</span>
+              </div>
+            </v-flex>
+
+            <v-flex xs3 sm1 md1 lg1 xl1>
+              <v-chip x-small ripple :class="`${todo.state} white--text caption ma-0 pa-0 px-2 py-2`">
+                <span class="caption">{{ todo.state }}</span>
+              </v-chip>
+            </v-flex>
+
+            <v-flex xs1 sm1 md1 lg1 xl1>
+              <v-btn class="mt-n1" @click="deleteTodo(todo)" icon>
+                  <v-icon small class="red--text text--darken-5"
+                    >mdi-trash-can</v-icon
+                  >
+                </v-btn>
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+        </v-card-text>
+      </v-card>
+    </transition-group>
   </v-container>
 </template>
 
@@ -156,23 +154,16 @@ export default {
         querySnapshot.forEach(doc => {
           // doc.data() is never undefined for query doc snapshots
           // console.log(doc.id, " => ", doc.data());
-          let docDate = doc.data().date;
-          // // console.log("docDate", docDate);
+          let docDate = doc.data().date; //document date
 
-          let currentDate = new Date().toISOString().substr(0, 10);
-          // // console.log("currentDate", currentDate);
+          let currentDate = new Date().toISOString().substr(0, 10); //current date
 
           if (docDate < currentDate) {
-            console.log("Task is Over due");
-            console.log(doc.id);
+            //checks if document date is less than current date
             this.$firestore.todos.doc(doc.id).update({
               state: "overdue"
             });
           }
-          // console.log(doc.id);
-          // this.$firestore.todos.doc(doc.id).update({
-          //   state: "overdue"
-          // })
         });
       })
       .catch(error => {
