8e15bff0e0a11d46af8e8ed94f7d76bf01982138
Rukkie555 <terryfrank555@gmail.com>
2019-12-18 09:36:23 +0100

    added time

--- src/components/dashboard/Dialog.vue
+++ src/components/dashboard/Dialog.vue
@@ -28,19 +28,21 @@
         </v-btn>
       </template>
       <v-card>
-        <v-card-title class="headline">Add a New Todo</v-card-title>
+        <v-card-title class="headline">Add a New Task</v-card-title>
         <v-card-text>
           <v-form class="px-3">
             <v-layout row wrap>
               <v-flex xs12 sm12 md12 lg12 xl12>
                 <v-text-field
-                  color="#9652ff"
+                  color="#3cd1c2"
+                  :clearable="true"
                   v-model="todo.title"
                   label="Task"
                   prepend-inner-icon="mdi-bookmark-check"
                 ></v-text-field>
               </v-flex>
-              <v-flex xs12 sm12 md12 lg12 xl12>
+
+              <v-flex xs6 sm6 md6 lg6 xl6>
                 <v-menu
                   v-model="todo.menu2"
                   :close-on-content-click="false"
@@ -54,15 +56,47 @@
                       v-model="todo.date"
                       label="Picker without buttons"
                       prepend-inner-icon="mdi-calendar"
+                      color="#3cd1c2"
                       readonly
                       v-on="on"
                     ></v-text-field>
                   </template>
                   <v-date-picker
                     v-model="todo.date"
+                    header-color="#3cd1c2"
                     @input="todo.menu2 = false"
                   ></v-date-picker>
                 </v-menu>
+              </v-flex>
+
+              <v-flex xs6 sm6 md6 lg6 xl6>
+                <v-dialog
+                  ref="dialog"
+                  v-model="modal2"
+                  :return-value.sync="todo.time"
+                  persistent
+                  width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      v-model="todo.time"
+                      label="Picker in dialog"
+                      prepend-icon="mdi-calendar-clock"
+                      color="#3cd1c2"
+                      readonly
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-time-picker header-color="#3cd1c2" v-if="modal2" v-model="todo.time" full-width>
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="modal2 = false"
+                      >Cancel</v-btn
+                    >
+                    <v-btn text color="primary" @click="$refs.dialog.save(todo.time)"
+                      >OK</v-btn
+                    >
+                  </v-time-picker>
+                </v-dialog>
               </v-flex>
             </v-layout>
           </v-form>
@@ -71,7 +105,7 @@
           <v-layout justify-center>
             <v-btn
               :loading="loading"
-              class="text-capitalize"
+              class="text-capitalize white--text"
               color="#9652ff"
               fab
               @click="submit"
@@ -91,9 +125,12 @@ import { fb, db } from "../../firebaseConfig";
 export default {
   data: () => ({
     dialog: false,
+    menu: false,
+    modal2: false,
     todo: {
       title: null,
       menu2: false,
+      time: null,
       date: new Date().toISOString().substr(0, 10),
       createdAt: new Date().toISOString().substr(0, 10),
       author: fb.auth().currentUser.uid,
